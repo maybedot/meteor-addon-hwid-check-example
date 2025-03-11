f6caca8d7d66045291e1e6f72780b0a55250f7d6
maybedot <alexanderceh22@mail.ru>
2025-03-11 05:19:16 +0300

    Create AddonTemplate.java

---
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -0,0 +1,84 @@
+package com.example.addon;
+
+import com.example.addon.commands.CommandExample;
+import com.example.addon.hud.HudExample;
+import com.example.addon.modules.ModuleExample;
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.GithubRepo;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class AddonTemplate extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("Example");
+    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Meteor Addon Template");
+        String hwid = HWID.getHWID();
+        LOG.info("HWID: " + hwid);
+        if (!isHwidValid(hwid)) {
+            LOG.info("HWID is not valid, shutting down...");
+            System.exit(1);
+            return;
+        }
+
+        // Modules
+        Modules.get().add(new ModuleExample());
+
+        // Commands
+        Commands.add(new CommandExample());
+
+        // HUD
+        Hud.get().register(HudExample.INFO);
+    }
+
+    private boolean isHwidValid(String hwid) {
+        try {
+            URL url = new URL("https://example.com/"); // Put there your website where you keeping your HWID`s.
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine).append("\n");
+            }
+
+            in.close();
+            connection.disconnect();
+            return content.toString().contains(hwid);
+        } catch (Exception e) {
+            LOG.error("Error checking HWID: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.example.addon";
+    }
+
+    @Override
+    public GithubRepo getRepo() {
+        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
+    }
+}
